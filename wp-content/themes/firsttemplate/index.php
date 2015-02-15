0f09cd73b09ae6b739c0ad7fcab5456fd2a6c27c
Anton Golomazov <agolomazov@itloft.ru>
2015-02-15 14:49:33 +0300

    Add title into tag title

--- wp-content/themes/firsttemplate/index.php
+++ wp-content/themes/firsttemplate/index.php
@@ -3,67 +3,12 @@
     <div class="content-main">
 
         <div class="content">
-
             <div id='slideshowHolder'>
-
                 <img src="<?php echo bloginfo('template_url') ?>/images/img1.jpg" alt='' />
-
                 <img src="<?php echo bloginfo('template_url') ?>/images/img1.jpg" alt='' />
-
                 <img src="<?php echo bloginfo('template_url') ?>/images/img1.jpg" alt='' />
-
             </div>
 
-            <div class="articles">
-
-                <div class="articles-gen-img">
-                    <a href="#"><img src="<?php echo bloginfo('template_url') ?>/images/post-img1.jpg" alt="Preview image" /></a>
-                </div>
-                <div class="articles-head">
-                    <span class="articles-date"><img src="<?php echo bloginfo('template_url') ?>/images/articles-autor.jpg" alt="admin" /> <span>Admin</span> - Nov 28th, 2010</span>
-                    <span class="articles-comments"><img src="<?php echo bloginfo('template_url') ?>/images/articles-comment.jpg" alt="commets" /> <a href="#">10 комментариев</a></span>
-                </div>
-
-                <h1><a href="#">Thanksgiving greeting card PSD</a></h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada orci a commodo. Aenean dapibus urna quis nulla consequat sagittis. Quisque ut ultrices massa. Mauris felis felis, rutrum sit amet vehicula ut, tempus quis lectus...</p>
-
-                <p><a href="#">Read More</a></p>
-
-            </div>
-
-            <div class="articles">
-
-                <div class="articles-gen-img">
-                    <a href="#"><img src="<?php echo bloginfo('template_url') ?>/images/post-img2.jpg" alt="Preview image" /></a>
-                </div>
-                <div class="articles-head">
-                    <span class="articles-date"><img src="<?php echo bloginfo('template_url') ?>/images/articles-autor.jpg" alt="admin" /> <span>Admin</span> - Nov 28th, 2010</span>
-                    <span class="articles-comments"><img src="<?php echo bloginfo('template_url') ?>/images/articles-comment.jpg" alt="commets" /> <a href="#">10 комментариев</a></span>
-                </div>
-
-                <h1><a href="#">Thanksgiving greeting card PSD</a></h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada orci a commodo. Aenean dapibus urna quis nulla consequat sagittis. Quisque ut ultrices massa. Mauris felis felis, rutrum sit amet vehicula ut, tempus quis lectus...</p>
-
-                <p><a href="#">Read More</a></p>
-
-            </div>
-
-            <div class="articles">
-
-                <div class="articles-gen-img">
-                    <a href="#"><img src="<?php echo bloginfo('template_url') ?>/images/post-img3.jpg" alt="Preview image" /></a>
-                </div>
-                <div class="articles-head">
-                    <span class="articles-date"><img src="<?php echo bloginfo('template_url') ?>/images/articles-autor.jpg" alt="admin" /> <span>Admin</span> - Nov 28th, 2010</span>
-                    <span class="articles-comments"><img src="<?php echo bloginfo('template_url') ?>/images/articles-comment.jpg" alt="commets" /> <a href="#">10 комментариев</a></span>
-                </div>
-
-                <h1><a href="#">Thanksgiving greeting card PSD</a></h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada orci a commodo. Aenean dapibus urna quis nulla consequat sagittis. Quisque ut ultrices massa. Mauris felis felis, rutrum sit amet vehicula ut, tempus quis lectus...</p>
-
-                <p><a href="#">Read More</a></p>
-
-            </div>
 
             <div class="articles">
 
